7699fef10deab57dcfced0b310774774ad5db492
shailendra <shail5788@gmail.com>
2019-01-31 21:12:16 +0530

    update some file

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ module.exports = {
   serverport: 1978,
   //    'tokenexp': 3600,
   secret: "mysecretkey",
+  server:"development",
   //'database': 'mongodb://localhost:27017/expensedb'
   database: "mongodb://shail5788:shail5788@ds121834.mlab.com:21834/blogger"
 };
